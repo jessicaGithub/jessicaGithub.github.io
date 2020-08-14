a730191bf2a306f8ccb5b6aca56bb5a574d3d8db
jessicaGithub <himechi90@gmail.com>
2020-08-14 13:32:25 +1000

    Updates

---
+++ precache-manifest.57b42f6dc5d58d4153a275d4f8cefe1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec3054eaad50a0f21f8f72ddeb5bdcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1adb31ef86d030aedc98",
+    "url": "/static/css/main.65071702.chunk.css"
+  },
+  {
+    "revision": "cf3d25057aac65fbbd25",
+    "url": "/static/js/2.121d4833.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.121d4833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1adb31ef86d030aedc98",
+    "url": "/static/js/main.972b8bba.chunk.js"
+  },
+  {
+    "revision": "2d08388512f8fde7c1f5",
+    "url": "/static/js/runtime-main.0cae8335.js"
+  },
+  {
+    "revision": "d4cf9debc450e2fe3f4d47d0354013a0",
+    "url": "/static/media/Sunset.d4cf9deb.mp4"
+  }
+]);
